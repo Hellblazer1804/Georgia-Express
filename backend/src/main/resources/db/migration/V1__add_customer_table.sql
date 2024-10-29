354b98254b93b11a6457b783231b015d3fe30481
Bandhan Patel <btp90678@uga.edu>
2024-10-29 16:55:03 -0400

    Added flyway stuff

---
+++ backend/src/main/resources/db/migration/V1__add_customer_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS customer(
+    customerId INTEGER,
+    name VARCHAR(255),
+    ssn VARCHAR(10),
+    creditScore INTEGER,
+    dob DATE,
+    address VARCHAR(255),
+    email VARCHAR(255),
+    phoneNumber VARCHAR(11),
+    salary INTEGER
+);
