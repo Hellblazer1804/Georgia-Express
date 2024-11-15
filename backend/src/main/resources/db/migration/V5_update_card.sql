a878d821666110dbd503de36b7f086156e1e4454
Hetpat02 <pathakhet2@gmail.com>
2024-11-15 15:31:34 -0500

    created card and login table

--- backend/src/main/resources/db/migration/V5_update_card.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE card
-    ADD CONSTRAINT pk_card PRIMARY KEY (cardNumber);
-
-ALTER TABLE card
-    ADD CONSTRAINT fk_card FOREIGN KEY (customer_id) REFERENCES customer (customer_id);
--- backend/src/main/resources/db/migration/V6_add_customer_login_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS customerLogin(
-    username VARCHAR(255),
-    password VARCHAR(255) NOT NULL,
-    customer_id INTEGER
-);
