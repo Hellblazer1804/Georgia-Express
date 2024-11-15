3b95425af2d32cad6c2f687d9362cdf379aa9d71
Hetpat02 <pathakhet2@gmail.com>
2024-11-15 15:12:46 -0500

    created card and login table

---
+++ backend/src/main/resources/db/migration/V4__add_card_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS card(
+    cardNumber INTEGER,
+    ExpirationDate DATE,
+    cvv INTEGER,
+    creditLimit INTEGER,
+    cardBalance INTEGER,
+    minimumPayment INTEGER,
+    rewardPoints INTEGER,
+    customer_id INTEGER
+);
