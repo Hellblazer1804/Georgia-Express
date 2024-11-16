f3ba3ba9c5ff1bf8dc79a84fe8d46d5fc3dff63c
Hetpat02 <pathakhet2@gmail.com>
2024-11-16 13:53:19 -0500

    Implemented Dias's comments.

--- backend/src/main/resources/db/migration/V4__add_card_table.sql
+++ backend/src/main/resources/db/migration/V4__add_card_table.sql
@@ -2,9 +2,9 @@ CREATE TABLE IF NOT EXISTS card(
     cardNumber INTEGER,
     ExpirationDate DATE,
     cvv INTEGER,
-    creditLimit INTEGER,
-    cardBalance INTEGER,
-    minimumPayment INTEGER,
-    rewardPoints INTEGER,
+    creditLimit NUMERIC(12, 2),
+    cardBalance NUMERIC(12, 2),
+    minimumPayment NUMERIC(8, 2),
+    rewardPoints NUMERIC(10, 2),
     customer_id INTEGER
 );
--- backend/src/main/resources/db/migration/V6__add_customer_login_table.sql
+++ backend/src/main/resources/db/migration/V6__add_customer_login_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS customerLogin(
-    username VARCHAR(255),
+    username VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     customer_id INTEGER
 );
