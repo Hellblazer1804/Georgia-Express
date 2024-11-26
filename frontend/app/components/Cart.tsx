caad9ce6b5e791480a4729c11f17df4f9ae42bb9
sonicisreal <mdr79855@uga.edu>
2024-11-25 23:07:30 -0500

    transaction stuff

--- frontend/app/components/Cart.tsx
+++ frontend/app/components/Cart.tsx
@@ -2,11 +2,12 @@
 import React, {useEffect, useState} from "react";
 import style from "./Cart.module.css";
 import {useSearchParams} from "next/navigation";
+import {useRouter} from "next/navigation";
 
 interface CartItem {
     cart_item_id: number;
     item_name: string;
-    cost: number;
+    cart_item_cost: number;
     quantity: number;
 }
 
@@ -15,6 +16,7 @@ export default function Cart() {
     const searchParams = useSearchParams();
     const customerId = searchParams.get("id");
     const customerUsername = searchParams.get("user");
+    const [totalCost, setTotalCost] = useState<number>(0);
 
     useEffect(() => {
         const fetchCartItems = async () => {
@@ -31,8 +33,10 @@ export default function Cart() {
                 }
                 const data = await response.json();
                 const cartItems : CartItem[] = data.cartItems;
+                const totalCost = data.cart_amount;
                 console.log("cart items", cartItems);
                 setCartItems(cartItems);
+                setTotalCost(totalCost);
             } catch (error) {
                 console.error("Error fetching cart items:", error);
             }
@@ -42,10 +46,39 @@ export default function Cart() {
     }, [customerUsername]);
 
     const handleCheckout = async () => {
-        // Handle checkout logic here
+        const creditCard = (document.querySelector('input[name="creditCard"]') as HTMLInputElement).value;
+        const cvv = (document.querySelector('input[name="cvv"]') as HTMLInputElement).value;
+        const cardInfo = {
+            card_number: creditCard.toString(),
+            cvv: parseInt(cvv, 10),
+        }
+
+        try {
+            const response = await fetch(`http://localhost:8080/api/transaction/process`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "username": customerUsername || "",
+                },
+                body: JSON.stringify(cardInfo),
+            });
+            console.log("response", response);
+            if (response.ok) {
+                alert("Checkout successful!");
+                useRouter().push(`/overview?id=${customerId}&user=${customerUsername}`);
+            } else {
+                const errorData = await response.json();
+                console.error("Error response:", errorData);
+                alert(errorData);
+            }
+        } catch (error) {
+            alert(error);
+            console.error("Error checking out:", error);
+        }
     };
 
     return (
+        <div className={style.cartPage}>
         <div className={style.cart}>
             <h1>Your Cart</h1>
             {cartItems.length === 0 ? (
@@ -53,10 +86,26 @@ export default function Cart() {
             ) : (
                 cartItems.map((item, index) => (
                     <div key={index}>
-                        {item.item_name}
+                        {item.item_name} - ${item.cart_item_cost} x {item.quantity}
                     </div>
                 ))
             )}
+            <div className={style.totalCost}>
+                Total: ${totalCost}
+            </div>
+            <a href={`/store?id=${customerId}&user=${customerUsername}`}>Back to store</a>
         </div>
+            <div className={style.checkout}>
+                <form onSubmit={handleCheckout}>
+                    <div className={style.inputBox}>
+                        <input name="creditCard" type="text" placeholder="Credit Card Number" required />
+                    </div>
+                    <div className={style.inputBox}>
+                        <input name={"cvv"} type="text" placeholder="CVV" required />
+                    </div>
+                    <button type="submit">Checkout</button>
+                </form>
+            </div>
+            </div>
     );
 }
