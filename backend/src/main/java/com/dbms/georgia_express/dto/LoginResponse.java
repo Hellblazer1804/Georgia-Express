8ddb43475842932aecb51516fbb50fb2556992c3
diasmashikov <diasmashikovnasa@gmail.com>
2024-11-30 18:07:38 -0500

    Added JWT Auth

---
+++ backend/src/main/java/com/dbms/georgia_express/dto/LoginResponse.java
@@ -0,0 +1,20 @@
+package com.dbms.georgia_express.dto;
+
+import com.dbms.georgia_express.model.CustomerLogin;
+
+public class LoginResponse {
+    private String token;
+
+    public LoginResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+}
